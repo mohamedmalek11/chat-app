379bdbbed0f239aa6315434aab5148a343655909
Mohamed Malek <mobile@matjrah.com>
2023-01-08 01:05:00 +0200

    fixed name float issue

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const style = {
-  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-full`,
-  name: `fixed mt-[-4rem] text-gray-600 text-xs`,
+  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-full relative`,
+  name: `absolute mt-[-4rem] text-gray-600 text-xs`,
   sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right
 rounded-bl-full`,
   received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,

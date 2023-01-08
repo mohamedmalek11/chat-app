5b30fc4b7434f43295ecfa4ee1d6bd16843159ce
Mohamed Malek <mobile@matjrah.com>
2023-01-08 02:37:44 +0200

    Added custom button & removed react button

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../firebase";
-import { GoogleButton } from "react-google-button";
 
 const style = {
   buttonWrapper: `flex justify-center`,
-  button: `bg-lime-800`,
+  button: `bg-lime-800 h-[3rem] text-white px-4 flex flex-row  items-center gap-1 rounded`,
+  svg: `w-[20px]`,
 };
 const Signin = () => {
   const googleSignIn = () => {
@@ -14,11 +14,41 @@ const Signin = () => {
   };
   return (
     <div className={style.buttonWrapper}>
-      <GoogleButton
-        onClick={googleSignIn}
-        className={style.button}
-        style={{ backgroundColor: "non" }}
-      />
+      <button onClick={googleSignIn} className={style.button}>
+        <svg
+          className={style.svg}
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 48 48"
+        >
+          <defs>
+            <path
+              id="a"
+              d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+            />
+          </defs>
+          <clipPath id="b">
+            <use href="#a" overflow="visible" />
+          </clipPath>
+          <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+          <path
+            clip-path="url(#b)"
+            fill="#EA4335"
+            d="M0 11l17 13 7-6.1L48 14V0H0z"
+          />
+          <path
+            clip-path="url(#b)"
+            fill="#34A853"
+            d="M0 37l30-23 7.9 1L48 0v48H0z"
+          />
+          <path
+            clip-path="url(#b)"
+            fill="#4285F4"
+            d="M48 48L17 24l-4-3 35-10z"
+          />
+        </svg>
+        <span>Sign with google</span>
+      </button>
     </div>
   );
 };

ed04edbb505f81c9c139190a590bbf1589bbdf34
Mohamed Malek <mobile@matjrah.com>
2023-01-08 12:58:21 +0200

    Fixed clipPath react issue

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -30,19 +30,19 @@ const Signin = () => {
           <clipPath id="b">
             <use href="#a" overflow="visible" />
           </clipPath>
-          <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
+          <path clipPath="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
           <path
-            clip-path="url(#b)"
+            clipPath="url(#b)"
             fill="#EA4335"
             d="M0 11l17 13 7-6.1L48 14V0H0z"
           />
           <path
-            clip-path="url(#b)"
+            clipPath="url(#b)"
             fill="#34A853"
             d="M0 37l30-23 7.9 1L48 0v48H0z"
           />
           <path
-            clip-path="url(#b)"
+            clipPath="url(#b)"
             fill="#4285F4"
             d="M48 48L17 24l-4-3 35-10z"
           />
